c74b37dbf9a1a5169cb52e8dae4c634d34ff71f5
utsavgada <utsav.gada@sakec.ac.in>
2021-02-25 11:40:33 +0530

    update 4.0

--- js/script.js
+++ js/script.js
@@ -1,34 +1,90 @@
 // Portfolio Item FIlter
 
-// const filterContainer=document.querySelector(".portfolio-filter"),
-// 	  filterBtns=filterContainer.children,
-// 	  totalFilterBtn=filterBtns.length,
-// 	  portfolioItems=document.querySelectorAll(".portfolio-items"),
-// 	  totalPortfolioItem=portfolioItems.length;
-	 
-	  
-// 	  for(let i=0; i<totalFilterBtn; i++){
-// 	  	filterBtns[i].addEventListener("click", function(){
-// 	  		filterContainer.querySelector(".active").classList.remove("active");
-// 	  		this.classList.add("active");
+const filterContainer=document.querySelector(".portfolio-filter"),
+	  filterBtns=filterContainer.children,
+	  totalFilterBtn=filterBtns.length,
+	  portfolioItems=document.querySelectorAll(".portfolio-item"),
+	  totalPortfolioItem=portfolioItems.length;
+	  for(let i=0; i<totalFilterBtn; i++){
+	  	filterBtns[i].addEventListener("click", function(){
+	  		filterContainer.querySelector(".active").classList.remove("active");
+	  		this.classList.add("active");
+	  		const filterValue=this.getAttribute("data-filter");
+	  		for (let k=0; k<totalPortfolioItem; k++){
+	  			if(filterValue === portfolioItems[k].getAttribute("data-catagory")){
+	  				portfolioItems[k].classList.remove("hide");
+	  				portfolioItems[k].classList.add("show");
+	  			}
+	  			else{portfolioItems[k].classList.remove("show");
+	  				portfolioItems[k].classList.add("hide");
+	  			}
+	  			if (filterValue === "all"){
+	  				portfolioItems[k].classList.remove("hide");
+	  				portfolioItems[k].classList.add("show");
+	  			}
+	  		}
+	  	})
+	  }
 
-// 	  		const filterValue=this.getAttribute("data-filter");
-// 	  		for (let k=0; k<totalPortfolioItem; k++){
-// 	  			if(filterValue === portfolioItems[k].getAttribute("data-category")){
-// 	  				portfolioItems[k].classList.remove("hide");
-// 	  				portfolioItems[k].classList.add("show");
-// 	  			}
-// 	  			else{
-// 	  				portfolioItems[k].classList.add("hide");
-// 	  			}
-// 	  		}
-// 	  	})
-// 	  }
+// Portfolio Lightbox
 
+const lightbox = document.querySelector(".lightbox"),
+	  lightboxImg = lightbox.querySelector(".lightbox-img"),
+	  lightboxClose = lightbox.querySelector(".lightbox-close"),
+	  lightboxText = lightbox.querySelector(".caption-text"),
+	  lightboxCounter = lightbox.querySelector(".caption-counter");
+let itemIndex = 0;
+for(let i=0; i<totalPortfolioItem; i++){
+	portfolioItems[i].addEventListener("click",function(){
+		itemIndex=i;
+		changeItem();
+		toggleLightbox();
+	})
+}
+
+function toggleLightbox(){
+	lightbox.classList.toggle("open");
+}
+
+function nextItem(){
+	if (itemIndex === totalPortfolioItem-1){
+		itemIndex=0;
+	}
+	else{
+		itemIndex++
+	}
+	changeItem();
+}
+
+function prevItem(){
+	if (itemIndex === 0){
+		itemIndex=totalPortfolioItem-1;
+	}
+	else{
+		itemIndex--;
+	}
+	changeItem();
+}
+
+
+function changeItem(){
+	imgSrc=portfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+	lightboxImg.src=imgSrc;
+	lightboxText.innerHTML=portfolioItems[itemIndex].querySelector("h4").innerHTML;
+	lightboxCounter.innerHTML=itemIndex+1 + " of " + totalPortfolioItem;
+}
+
+// close lightbox
+
+lightbox.addEventListener("click",function(event){
+	if(event.target === lightboxClose || event.target === lightbox){
+		toggleLightbox();
+	}
+})
 
 // Testimonial slider
 
-(() =>{
+
 
 	const sliderContainer = document.querySelector(".testi-slider-container"),
 	slides = sliderContainer.querySelectorAll(".testi-item");
@@ -37,14 +93,10 @@
 	nextBtn = document.querySelector(".testi-slider-nav .next");
 	activeSlide = sliderContainer.querySelector(".testi-item.active");
 	let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
-	
-
-
 	slides.forEach((slide) =>{
 		slide.style.width = slideWidth + "px";
 	})
 
-	
 	sliderContainer.style.width = slideWidth * slides.length + "px";
 
 	nextBtn.addEventListener("click", ()=>{
@@ -73,5 +125,3 @@
 	}
 	slider();
 
-
-})();
